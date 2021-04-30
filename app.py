6f21b47076cb72ee4075e8dd5da9c2844d09cbeb
Juhi Aggarwal <juhiaggarwal@Juhis-MacBook-Pro.local>
2021-04-30 17:21:21 -0500

    flask code for stations and temps

---
+++ app.py
@@ -0,0 +1,84 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+base = automap_base()
+
+#reflecting the database
+base.prepare(engine, reflect=True)
+
+#Save your references for each table
+Measurement = base.classes.measurement
+Station = base.classes.station
+
+#link to python database
+session = Session(engine)
+
+#Set up Flask
+
+#creating a Flask application called "app"
+app = Flask(__name__)
+#define route
+@app.route("/")
+
+def welcome():
+    return(
+    ''''Welcome to the Climate Analysis API!
+    Available Routes:'
+    '/api/v1.0/precipitation\n'
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/emp/start/end
+    ''')
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    #Calculate the date one year ago from the last date in the DB
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        # Astrick represents multiple entries for min, avg, and max temps
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
